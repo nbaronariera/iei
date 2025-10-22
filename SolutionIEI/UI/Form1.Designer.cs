d6c4473b92148806d4af1c3629c3a6fea4142c48
Nacho Trujillo Reillo <NTRUREI@upv.edu.es>
2025-10-22 18:43:10 +0200

    ahora sí que si el mapa

---
+++ SolutionIEI/UI/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows.Forms;
+
+namespace UI.UI_Gestor
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 0); // Será ajustado dinámicamente
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 18;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(300, 300); // Tamaño inicial, será ajustado
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 12D;
+            this.gMapControl1.Anchor = AnchorStyles.Top | AnchorStyles.Right; // Anclar arriba-derecha
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1100, 836);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+    }
+}
---
+++ SolutionIEI/UI/Form1.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using System.Linq;
+
+namespace UI.UI_Gestor
+{
+    public partial class Form1 : Form
+    {
+        private GMapOverlay markersOverlay;
+        private GMapOverlay routeOverlay;
+
+        private Dictionary<string, PointLatLng> Coordinates = new Dictionary<string, PointLatLng>()
+        {
+            { "Prueba", new PointLatLng(39.4702393, -0.3768049) },
+            { "Prueba2", new PointLatLng(39.4762777, -0.4191981) },
+            { "Prueba3", new PointLatLng(39.4038783, -0.4034584) },
+            { "Prueba4", new PointLatLng(39.42429, -0.38285) }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            gMapControl1.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gMapControl1.ShowCenter = false;
+
+
+            markersOverlay = new GMapOverlay("markers");
+            routeOverlay = new GMapOverlay("route");
+            gMapControl1.Overlays.Add(markersOverlay);
+            gMapControl1.Overlays.Add(routeOverlay);
+
+
+            gMapControl1.MinZoom = 2;
+            gMapControl1.MaxZoom = 18;
+            gMapControl1.Zoom = 12;
+
+
+            gMapControl1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+
+
+            UpdateMapSize();
+
+
+            if (Coordinates.Any())
+            {
+                var avgLat = Coordinates.Values.Average(p => p.Lat);
+                var avgLng = Coordinates.Values.Average(p => p.Lng);
+                gMapControl1.Position = new PointLatLng(avgLat, avgLng);
+            }
+            else
+            {
+                gMapControl1.Position = new PointLatLng(39.4699, -0.3763);
+            }
+
+
+            this.Resize += new EventHandler(Form1_Resize);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                CargarMarcadores();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el mapa: " + ex.Message);
+            }
+        }
+
+        private void CargarMarcadores()
+        {
+            foreach (var coordenada in Coordinates)
+            {
+                var marcador = new GMarkerGoogle(coordenada.Value, GMarkerGoogleType.red_small);
+                marcador.ToolTipText = coordenada.Key;
+                markersOverlay.Markers.Add(marcador);
+            }
+            gMapControl1.Refresh();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            UpdateMapSize();
+        }
+
+        private void UpdateMapSize()
+        {
+
+            int margin = 20;
+            int mapWidth = (int)(this.ClientSize.Width * 0.5);
+            int mapHeight = (int)(this.ClientSize.Height * 0.5);
+
+
+            mapWidth = Math.Max(mapWidth, 300);
+            mapHeight = Math.Max(mapHeight, 300);
+
+            // Establecer tamaño
+            gMapControl1.Size = new Size(mapWidth, mapHeight);
+
+            // Posicionar en la esquina superior derecha
+            gMapControl1.Location = new Point(this.ClientSize.Width - mapWidth - margin, margin);
+        }
+    }
+}
