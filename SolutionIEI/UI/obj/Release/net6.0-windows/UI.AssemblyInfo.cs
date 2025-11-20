7601dc545f01578997cab2563ee636109f845305
bayronrojass <bayronrojas30@gmail.com>
2025-11-20 18:32:12 +0100

    Selenium

--- SolutionIEI/UI/obj/Release/net6.0-windows/UI.AssemblyInfo.cs
+++ SolutionIEI/UI/obj/Release/net6.0-windows/UI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8cba4d73ee84dbbcebdcf47e0747271594f58d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
