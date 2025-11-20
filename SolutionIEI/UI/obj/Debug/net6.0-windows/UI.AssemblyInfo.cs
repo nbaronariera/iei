df3c415379108af259e7b052cf4793ad91a50ab8
bayronrojass <bayronrojas30@gmail.com>
2025-11-20 19:20:08 +0100

    JSONConverter

--- SolutionIEI/UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
+++ SolutionIEI/UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7601dc545f01578997cab2563ee636109f845305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378806aa72bc9b096c2b517b4256d1b997e4db81")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
