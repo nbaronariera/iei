f3f57065ff3955c9e365c370ad458abab88f01e7
Nacho Trujillo Reillo <NTRUREI@upv.edu.es>
2025-11-21 16:49:10 +0100

    Añadido csv de la entrega

--- SolutionIEI/UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
+++ SolutionIEI/UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378806aa72bc9b096c2b517b4256d1b997e4db81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea3cf637bee42ee6dd514f494240534376fd2f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
