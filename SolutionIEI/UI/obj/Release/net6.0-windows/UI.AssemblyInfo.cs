3fed1117a09ad18128d8c04eaa9a6c79c075bd1e
Lucasm4rt <Lucasmartmart333@gmail.com>
2025-11-22 23:51:47 +0100

    Filtro

--- SolutionIEI/UI/obj/Release/net6.0-windows/UI.AssemblyInfo.cs
+++ SolutionIEI/UI/obj/Release/net6.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56900e7ef00705b95d7389692c682ecf130cbb63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55aaa40a6646f60225d42148a8792e16b6aa05dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
