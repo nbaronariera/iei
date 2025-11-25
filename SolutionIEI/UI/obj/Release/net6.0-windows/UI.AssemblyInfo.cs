a9c48bb666c0210f412692b788445cdd20c9613a
bayronrojass <bayronrojas30@gmail.com>
2025-11-25 16:36:52 +0100

    Ejecucion

--- SolutionIEI/UI/obj/Release/net6.0-windows/UI.AssemblyInfo.cs
+++ SolutionIEI/UI/obj/Release/net6.0-windows/UI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db32030c37f331cc5547dfe56caddb25107c7688")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
