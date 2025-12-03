d9dcf693bdb180cb8bd79b119e9623c49b1b0eb9
Nacho Trujillo Reillo <NTRUREI@upv.edu.es>
2025-12-03 23:42:07 +0100

    .

--- SolutionIEI/UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
+++ SolutionIEI/UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd233ebaabd47eb27c7c72904df27f954fec8091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcedd77cb572c19758574596e6387d01802accf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
