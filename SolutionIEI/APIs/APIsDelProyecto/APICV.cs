9de94b650ec7ea35c18b961e337c50d8d3bb2ab8
nbarrie <nbarrie@etsinf.upv.es>
2025-12-25 12:21:14 +0100

    No si aun me dejo sin subir las cosas

---
+++ SolutionIEI/APIs/APIsDelProyecto/APICV.cs
@@ -0,0 +1,40 @@
+﻿// Controllers/APIBusqueda.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UI.Entidades;
+using UI.Logica;
+
+namespace APIs.Controllers
+{
+    [ApiController]
+    [Route("/gal")]
+    public class APICV : ControllerBase
+    {
+        private readonly LogicaParseo _logica;
+        public APICV(LogicaParseo logica) => _logica = logica;
+
+        /// <summary>
+        /// Obtiene el json de la base de datos de la Comunidad Valenciana
+        /// </summary>
+        /// 
+        ///<returns> Devuelve el json </returns> 
+        ///<response code="200">Retorna el json</response>
+        [HttpGet("json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult getJSON()
+        {
+            try
+            {
+                var lista = _logica.loadCV();
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[API] ERROR en gal/GetJSON: {ex.Message}\n{ex.StackTrace}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ SolutionIEI/UI/Logica/LogicaParseo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Entidades;
+using UI.Parsers;
+using UI.Parsers.ParsedObjects;
+using UI.Wrappers;
+
+namespace UI.Logica
+{
+    public class LogicaParseo
+    {
+        public string loadCat() {
+            return XMLaJSONConversor.Ejecutar();
+        }
+
+        public string loadCV()
+        {
+            return JSONCoordenadas.Ejecutar();
+        }
+
+        public string loadGal()
+        {
+
+            return CSVaJSONConversor.Ejecutar();
+        }
+    }
+}
